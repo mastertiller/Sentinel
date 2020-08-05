36623292fcbb28a54a187e2c09fdc3a713321248
mastertiller <weishuitian@gmail.com>
2020-08-05 20:41:43 +0800

    test for timer and counter

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/block/qlearning/QLearningSlot.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/block/qlearning/QLearningSlot.java
@@ -3,16 +3,32 @@ package com.alibaba.csp.sentinel.slots.block.qlearning;
 import com.alibaba.csp.sentinel.Constants;
 import com.alibaba.csp.sentinel.context.Context;
 import com.alibaba.csp.sentinel.node.DefaultNode;
+import com.alibaba.csp.sentinel.qlearning.QInfo;
+import com.alibaba.csp.sentinel.qlearning.QLearningLearner;
+import com.alibaba.csp.sentinel.qlearning.QLearningMetric;
 import com.alibaba.csp.sentinel.slotchain.AbstractLinkedProcessorSlot;
 import com.alibaba.csp.sentinel.slotchain.ResourceWrapper;
 import com.alibaba.csp.sentinel.slots.system.SystemBlockException;
+import sun.plugin.dom.html.HTMLFormElement;
 
 public class QLearningSlot extends AbstractLinkedProcessorSlot<DefaultNode> {
+    private final QLearningLearner qLearningLearner = new QLearningLearner();
+
+    private final int batchNum = 1000;
 
     @Override
     public void entry(Context context, ResourceWrapper resourceWrapper, DefaultNode node, int count, boolean prioritized, Object... args)
             throws Throwable {
+        try{
+            qLearningLearner.learn();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
 
+        if(qLearningMetric.getAction() == 0){
+            throw new SystemBlockException(resourceWrapper.getName(), "q-learning");
+        }
         fireEntry(context, resourceWrapper, node, count, prioritized, args);
     }
 
