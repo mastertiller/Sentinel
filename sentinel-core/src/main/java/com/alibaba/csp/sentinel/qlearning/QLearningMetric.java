eba0e3c231e54f313316b605911775d508bd69fd
mastertiller <weishuitian@gmail.com>
2020-08-05 20:48:08 +0800

    test for timer and counter

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/qlearning/QLearningMetric.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/qlearning/QLearningMetric.java
@@ -70,6 +70,53 @@ public class QLearningMetric {
 
     /////////////////////////////方法//////////////////////////////////////
 
+    /**
+     * 通过Cpu使用率来判断当前所处的状态，返回值为qlearning算法里的当前状态
+     * Judging current state by cpu usage，it is current state which is in the Qlearning algorithm。
+     *
+     * @return
+     */
+    public synchronized String locateState(double currentCpuUsage, double currentLoad, double totalQps, double Rt, int curThreadNum) {
+        int stateC;
+        if (ifCheckCPU) {
+            stateC = new Double(currentCpuUsage / 0.1).intValue();
+        } else {
+            stateC = -10;
+        }
+//        int stateL = new Double(currentLoad / 0.1).intValue();
+        int stateQ = new Double(totalQps / 100).intValue();
+        int stateR = new Double(Rt / 5).intValue();
+        int stateT = new Double(curThreadNum / 50).intValue();
+
+        /**0.1
+         * 这里的stateindex是int类型 需要调整 以便对应后面的else情况
+         */
+
+        String currentState = stateC + "#" + stateQ + "#" + stateR + "#" + stateT;
+
+        if (!Qtable.containsKey(currentState)) {
+            if (!isTrain()) {
+                newStateCount++;
+//                System.out.print("new State: " + currentState);
+            }
+//            System.out.println("training - new State: " + currentState);
+            Qtable.put(currentState, new double[actionsCount]);
+            //返回current state
+        } else if (Qtable.containsKey(currentState)) {//这里需要考虑再加一些其他因果 但是我暂时没想好 等结合了那个新的code再去改好了
+            if (!isTrain()) {
+                oldStateCount++;
+//                System.out.print("old State: " + currentState);
+            }
+//            System.out.println("training - old State: " + currentState);
+//             stateIndex = Qtable.get(currentState);
+        } else {
+            System.out.println("Error in get state from Q table.");
+        }
+//        this.state = currentState;
+        return currentState;
+    }
+
+
 
     private static class QLearningMetricContainer {
         private static QLearningMetric instance = new QLearningMetric();
