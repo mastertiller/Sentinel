3a3c4fb2a3f1a88c70af7c6058c4a658fb6ff508
mastertiller <weishuitian@gmail.com>
2020-08-07 23:32:28 +0800

    test for learning

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/qlearning/QLearningLearner.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/qlearning/QLearningLearner.java
@@ -1,30 +1,39 @@
 package com.alibaba.csp.sentinel.qlearning;
 
+import com.alibaba.csp.sentinel.Constants;
 import com.alibaba.csp.sentinel.node.DefaultNode;
+import com.alibaba.csp.sentinel.slots.system.SystemRuleManager;
 import com.alibaba.csp.sentinel.util.TimeUtil;
 
 public class QLearningLearner {
     QLearningMetric qLearningMetric = new QLearningMetric().getInstance();
-    QInfo qInfo;
+    private int bacthNum = 200;
+    private long batchTime = 20;
 
-    public void learn(DefaultNode node) {
+//    QInfo qInfo = new QInfo();
+
+    /**
+    加入synchronized？？
+     */
+    public synchronized void learn(DefaultNode node) {
         if (checkUpdate()) {
-            //执行action
+            //每批第一个线程开始计时和计数
             System.out.println("start tick —— " + qLearningMetric.getCt()
                     + "    start count —— " + qLearningMetric.getCn());
+
             if(containsQInfo()){
                 //取，
                 //更新
-                UpdateQ();
+                UpdateQ(node);
                 System.out.println("取走： " + qLearningMetric.getBi() + "    " + qLearningMetric.getAction());
             }
             //开始下一批决策
-            qLearningMetric.addBi();
+            int bi = qLearningMetric.addBi();
             //决策
-            qInfo = takeAction(node);
+            QInfo qInfo = takeAction(node);
             //存
-            qLearningMetric.putHm(qLearningMetric.getBi(),qInfo);
-            System.out.println("存下： " + qLearningMetric.getBi() + "   " + qLearningMetric.getAction());
+            qLearningMetric.putHm(bi,qInfo);
+
         }
     }
 
@@ -41,11 +50,11 @@ public class QLearningLearner {
 //            + "    start count —— " + qLearningMetric.getCn());
             return true;
         }
-        if(t-ct >= 20 || qLearningMetric.getCn() >= 200){
+        if(t-ct >= batchTime || qLearningMetric.getCn() >= bacthNum){
 
             System.out.println("bombing! —— " + (t-ct)
                     + "    total count —— " + qLearningMetric.getCn());
-//            System.out.println(" ____________________________________________");
+
             qLearningMetric.setCt(TimeUtil.currentTimeMillis());
             qLearningMetric.resetCn();
             return true;
@@ -66,39 +75,49 @@ public class QLearningLearner {
         }
         else{
             //测试
-            System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            System.out.println("@@@@@ containsQInfo : 没存进去 @@@@@@ " + bi);
         }
         return false;
     }
 
-    private void UpdateQ() {
+    private synchronized void UpdateQ(DefaultNode node) {
 
-        qInfo = qLearningMetric.pushQInfo();
+        QInfo qInfo = qLearningMetric.pushQInfo();
         String s = qInfo.getState();
         int a = qInfo.getAction();
         double u = qInfo.getUtility();
-        double nextUtility = qLearningMetric.calculateUtility(cpu,rt,...);
+
+        double nextUtility = qLearningMetric.calculateUtility(node.successQps(),node.avgRt());
+
         int r = qLearningMetric.getReward(u,nextUtility);
         double q = qLearningMetric.getQValue(s,a);
-        double maxQ = qLearningMetric.getMaxQ();
-        double qUpdated = qLearningMetric.qUpdate(q,r,maxQ);
+        double maxQ = qLearningMetric.getMaxQ(SystemRuleManager.getCurrentCpuUsage(),node.passQps(),node.avgRt(),node.curThreadNum());
+        double qUpdated = qLearningMetric.updateQ(q,r,maxQ);
+        System.out.println("  NEXT batch: " + qLearningMetric.getBi() + " --------- NEXT successQPS: " + node.successQps() + " ======= NEXT RT: "  + node.avgRt() + " ======= NEXT Utility : " + nextUtility);
+         System.out.println(" ======= q :" + q + " ======== REWARD : " + r + " ======== maxQ : " + maxQ + " ======= q update : " + qUpdated );
         qLearningMetric.setQValue(s,a,qUpdated);
     }
 
     private synchronized QInfo takeAction(DefaultNode node) {
 
-        String s = qLearningMetric.locateState(node);
+        String s = qLearningMetric.locateState(SystemRuleManager.getCurrentCpuUsage(),node.passQps(),node.avgRt(),node.curThreadNum());
         int a;
         if(qLearningMetric.isTrain()){
             //随机选择
             a = qLearningMetric.getRandomAction();
+
         }
         else{
             //从qtable中选择
-            a = qLearningMetric.policy();
+            a = qLearningMetric.policy(s);
         }
+        /**
+         * TO DO: 尽量并入上面两种方法内
+         */
         qLearningMetric.setAction(a);
-        double u = qLearningMetric.calculateUtility(node);
+
+        double u = qLearningMetric.calculateUtility(node.successQps(),node.avgRt());
+        System.out.println(" last batch: " + qLearningMetric.getBi() + " --------- ThreadNum: " + Constants.ENTRY_NODE.curThreadNum() +  " ======== successQPS: " + node.successQps() + " ======= RT: "  + node.avgRt() + " ======= Utility : " + u);
 
 //        测试用
 //        String s = "x";
@@ -108,7 +127,8 @@ public class QLearningLearner {
 //        if(a > 1 || a < 0){
 //            System.out.println("--------------------- error in a ");
 //        }
-        qLearningMetric.setAction(a);
+//        qLearningMetric.setAction(a);
+
         QInfo qInfo = new QInfo();
         qInfo.setQInfo(s,a,u);
 
