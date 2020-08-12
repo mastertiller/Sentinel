15c860c3d794114c8e3e94701c9d0b3396583e4b
mastertiller <weishuitian@gmail.com>
2020-08-12 22:26:21 +0800

    sort

--- sentinel-demo/sentinel-demo-spring-webmvc/src/main/java/com/alibaba/csp/sentinel/demo/spring/webmvc/TestForSort.java
+++ sentinel-demo/sentinel-demo-spring-webmvc/src/main/java/com/alibaba/csp/sentinel/demo/spring/webmvc/TestForSort.java
@@ -1,13 +1,18 @@
 package com.alibaba.csp.sentinel.demo.spring.webmvc;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 public class TestForSort {
     public static void main(String[] args) {
         int num = 50000;
-        int[] randomList = new int[num];
+        Random random = new Random();
+        List<Integer> randomList = new ArrayList<>();
         for (int i = 0;i < num;i++){
-            randomList[i] = j;
+            randomList.add(random.nextInt(num));
         }
         Collections.sort(randomList);
     }
