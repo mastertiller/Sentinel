2c18f340f3ceb6d0014041bc01d5ca6297dfc207
mastertiller <weishuitian@gmail.com>
2020-08-16 15:15:41 +0800

    Optimize code structure.

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/qlearning/QLearningMetric.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/qlearning/QLearningMetric.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alibaba.csp.sentinel.qlearning;
 
 import java.util.Map;
@@ -6,22 +21,21 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
+/**
+ * @author ZhouYanjun
+ */
 public class QLearningMetric {
-    /**
-     * 注意：原子类变量不能直接用！如this.bi，应该用getBi()！
-     */
-    ///////////////////////需要检查是否上锁的变量/////////////////////////
+
     public boolean ifCheckCPU = true;
     public final int maxTrainNum = 50000000;
 
     public boolean isLearning;
 
-    final int[] actionValues = new int[]{0, 1};
-    String[] actionNames = new String[]{"Block", "Accept"};
+    private final int[] actionValues = new int[]{0, 1};
+    private String[] actionNames = new String[]{"Block", "Accept"};
     private volatile int actionsCount = actionValues.length;
 
     private volatile ConcurrentHashMap<String, double[]> Qtable = new ConcurrentHashMap<>();
-
 
     private final double alpha = 0.1;//alpha控制了效用方程的qps的参数
     private final double beta = 0.01;//控制了效用方程的RT的参数
@@ -39,83 +53,15 @@ public class QLearningMetric {
     private final int ThreadInterval = 2;
     private int statesCount;
 
-
-    ///////////////////////计时与计数器///////////////////
-    public long getCt() {
-        return ct.get();
-    }
-
-    public void setCt(long t) {
-        ct.set(t);
-    }
-
-    public int getCn() {
-        return cn.get();
-    }
-
-    public void addCn() {
-        cn.incrementAndGet();
-    }
-
-    public void resetCn(){
-        cn.set(0);
-    }
     private AtomicLong ct = new AtomicLong(0);
     private AtomicInteger cn = new AtomicInteger(0);
 
-    public int getBi() {
-        return bi.get();
-    }
-
-    public int addBi() {
-        return bi.incrementAndGet();
-    }
-    public void setBi(int i){
-        bi.set(i);
-    }
-
     private AtomicInteger bi = new AtomicInteger(0);
-
-    /////////////////////////保存QInfo////////////////////////////////
-
-    public ConcurrentHashMap<Integer,QInfo > getHm() {
-        return hm;
-    }
-
-    public void putHm(int i,QInfo qInfo) {
-        this.hm.put(i,qInfo);
-    }
-
-    public QInfo pushQInfo() {
-        if(!getHm().containsKey(getBi())){
-            System.out.println("   pushQInfo: 不存在");
-        }
-        QInfo qInfo = getHm().get(getBi());
-//        this.hm.remove(this.bi);
-        return qInfo;
-    }
 
     private ConcurrentHashMap<Integer, QInfo> hm = new ConcurrentHashMap<>();
 
-    ////////////////////////////action//////////////////////////
-
-    public int getAction(){
-        return this.action.get();
-    }
-    public void setAction(int a) {
-        this.action.set(a);
-    }
-
     private AtomicInteger action = new AtomicInteger(1);
 
-    /////////////////////////////方法//////////////////////////////////////
-
-    /**
-     * 通过Cpu使用率来判断当前所处的状态，返回值为qlearning算法里的当前状态
-     * Judging current state by cpu usage，it is current state which is in the Qlearning algorithm。
-     *
-     * @return
-     */
     public String locateState(double currentCpuUsage, double totalQps, double Rt, int curThreadNum) {
         int stateC;
         if (ifCheckCPU) {
@@ -123,7 +69,7 @@ public class QLearningMetric {
         } else {
             stateC = -10;
         }
-//        int stateL = new Double(currentLoad / 0.1).intValue();
+
         int stateQ = new Double(totalQps / QpsInterval).intValue();
         int stateR = new Double(Rt / RtInterval).intValue();
         int stateT = new Double(curThreadNum / ThreadInterval).intValue();
@@ -131,24 +77,11 @@ public class QLearningMetric {
         String currentState = stateC + "#" + stateQ + "#" + stateR + "#" + stateT;
 
         if (!Qtable.containsKey(currentState)) {
-            //测试用
-//            if (!isTrain()) {
-//                newStateCount++;
-//                System.out.print("new State: " + currentState);
-//            }
-//            System.out.println("training - new State: " + currentState);
+
             Qtable.put(currentState, new double[actionsCount]);
-            //返回current state
-        } else if (Qtable.containsKey(currentState)) {
-            //测试用
-//            if (!isTrain()) {
-//                oldStateCount++;
-//                System.out.print("old State: " + currentState);
-//            }
-//            System.out.println("training - old State: " + currentState);
-        } else {
-            System.out.println("Error in get state from Q table.");
+
         }
+
         return currentState;
     }
 
@@ -182,12 +115,11 @@ public class QLearningMetric {
         } else {
             stateC = -10;
         }
-//        int stateL = new Double(currentLoad / 0.1).intValue();
+
         int stateQ = new Double(totalQps / QpsInterval).intValue();
         int stateR = new Double(Rt / RtInterval).intValue();
         int stateT = new Double(curThreadNum / ThreadInterval).intValue();
 
-//        String nextState = stateC + "#" + stateL + "#" + stateQ + "#" + stateR + "#" + stateT;
         String nextState = stateC + "#" + stateQ + "#" + stateR + "#" + stateT;
         if (!Qtable.containsKey(nextState)) {
             return 0;
@@ -216,19 +148,6 @@ public class QLearningMetric {
             }
         }
         return policyGotoAction;
-    }
-
-    public synchronized void showPolicy() {
-        String fromState;
-        int toAction;
-        statesCount = Qtable.size();
-        System.out.println("\n ======= Show Policy =======" + statesCount);
-
-        for (Map.Entry entry : Qtable.entrySet()) {
-            fromState = (String) entry.getKey();
-            toAction = policy(fromState);
-            System.out.println("Current State: " + fromState + "       Action: " + actionNames[toAction] + "        Q Value: " + this.Qtable.get(fromState)[toAction]);
-        }
     }
 
     public synchronized int getRandomAction() {
@@ -262,6 +181,57 @@ public class QLearningMetric {
         return Qtable;
     }
 
+    public long getCt() {
+        return ct.get();
+    }
+
+    public void setCt(long t) {
+        ct.set(t);
+    }
+
+    public int getCn() {
+        return cn.get();
+    }
+
+    public void addCn() {
+        cn.incrementAndGet();
+    }
+
+    public void resetCn(){
+        cn.set(0);
+    }
+
+    public int getBi() {
+        return bi.get();
+    }
+
+    public int addBi() {
+        return bi.incrementAndGet();
+    }
+    public void setBi(int i){
+        bi.set(i);
+    }
+
+    public ConcurrentHashMap<Integer,QInfo > getHm() {
+        return hm;
+    }
+
+    public void putHm(int i,QInfo qInfo) {
+        this.hm.put(i,qInfo);
+    }
+
+    public QInfo pushQInfo() {
+        QInfo qInfo = getHm().get(getBi());
+        return qInfo;
+    }
+
+    public int getAction(){
+        return this.action.get();
+    }
+    public void setAction(int a) {
+        this.action.set(a);
+    }
+
     private static class QLearningMetricContainer {
         private static QLearningMetric instance = new QLearningMetric();
     }
@@ -269,7 +239,5 @@ public class QLearningMetric {
     public static QLearningMetric getInstance() {
         return QLearningMetricContainer.instance;
     }
-
-
 
 }
